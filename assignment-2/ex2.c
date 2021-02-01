be161b5a97c1a43324df72684e2ca8af8881e56f
morellam-dev <4561733+morellam-dev@users.noreply.github.com>
2021-02-01 12:24:50 -0500

    Rename identifiers in C program to use snake_case.

--- assignment-2/ex2.c
+++ assignment-2/ex2.c
@@ -15,27 +15,27 @@ int main() {
   const size_t size = sizeof(numbers) / sizeof(numbers[0]);
 
   // Calculate print maximum value
-  int highest = myMax(numbers, size);
+  int highest = my_max(numbers, size);
   printf("The highest number: %d\n", highest);
 
   // Calculate and print average
-  double avg = myAvg(numbers, size);
+  double avg = my_avg(numbers, size);
   printf("The average of the numbers: %g\n", avg);
 
   // Make a copy of the array
-  int* outNumbers = myCopy(numbers, size);
+  int* out_numbers = my_copy(numbers, size);
   // Print the copied array
   printf("The original array: ");
-  printNumbers(outNumbers, size);
+  print_numbers(out_numbers, size);
 
   // Reverse the array in place, and print it
-  myReverse(outNumbers, size);
+  my_reverse(out_numbers, size);
   printf("The reverse array: ");
-  printNumbers(outNumbers, size);
+  print_numbers(out_numbers, size);
 
   // Print the prime numbers in the array.
   printf("The prime numbers in the array: ");
-  printPrimeNumbers(outNumbers, size);
+  print_prime_numbers(out_numbers, size);
 
   printf("\n");
   return 0;
@@ -43,30 +43,30 @@ int main() {
 
 // FUNCTION IMPLEMENTATIONS (defined and documented in ex2.h)
 
-int myMax(int a[], size_t size) {
+int my_max(int a[], size_t size) {
   int max = a[0];
   for (int i = 1; i < size; i++)
     if (a[i] > max) max = a[i];
   return max;
 }
-double myAvg(int a[], size_t size) {
+double my_avg(int a[], size_t size) {
   if (size == 0) return 0;
   double sum = 0;
   for (int i = 0; i < size; i++) sum += a[i];
   return sum / size;
 }
-int* myCopy(int a[], size_t size) {
+int* my_copy(int a[], size_t size) {
   int* copy = (int*)malloc(size * sizeof(int));
   for (int i = 0; i < size; i++) {
     copy[i] = a[i];
   }
   return copy;
 }
-void printNumbers(int a[], size_t size) {
+void print_numbers(int a[], size_t size) {
   for (int i = 0; i < size; i++) printf("%d ", a[i]);
   printf("\n");
 }
-void myReverse(int a[], size_t size) {
+void my_reverse(int a[], size_t size) {
   int i = 0, j = size;
   for (int i = 0, j = size - 1; i < j; i++, j--) {
     int t = a[i];
@@ -80,7 +80,7 @@ bool isPrime(long num) {
     if (num % i == 0) return false;
   return true;
 }
-void printPrimeNumbers(int a[], size_t size) {
+void print_prime_numbers(int a[], size_t size) {
   for (int i = 0; i < size; i++) {
     if (isPrime(a[i])) printf("%d ", a[i]);
   }
--- assignment-2/ex2.h
+++ assignment-2/ex2.h
@@ -8,20 +8,20 @@
 
 // Returns the largest integer in the array
 // Precondition: size > 0, a has at least `size` elements
-int myMax(int a[], size_t size);
+int my_max(int a[], size_t size);
 
 // Returns the average of the integers in the array
 // Precondition: size > 0, a has at least `size` elements
-double myAvg(int a[], size_t size);
+double my_avg(int a[], size_t size);
 
 //
-int* myCopy(int a[], size_t size);
+int* my_copy(int a[], size_t size);
 
 //
-void printNumbers(int a[], size_t size);
+void print_numbers(int a[], size_t size);
 
 // Reverse the array in place.
-void myReverse(int a[], size_t size);
+void my_reverse(int a[], size_t size);
 
 // Returns whether num is a prime number
 // i.e. there exists no integer i > 2 such that num/i is an integer.
@@ -29,4 +29,4 @@ void myReverse(int a[], size_t size);
 bool isPrime(long num);
 
 //
-void printPrimeNumbers(int a[], size_t size);
+void print_prime_numbers(int a[], size_t size);
