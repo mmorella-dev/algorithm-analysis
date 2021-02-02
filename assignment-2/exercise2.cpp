3493026ff9f74b12849243255d249869cfc5bdcf
morellam-dev <4561733+morellam-dev@users.noreply.github.com>
2021-02-02 15:18:39 -0500

    Reduce C++ to STL-only implementation

--- assignment-2/exercise2.cpp
+++ assignment-2/exercise2.cpp
@@ -1,8 +1,8 @@
 // FILE: exercise2.cpp
 // AUTHOR: mae morella
 
-#include "exercise2.hpp"
 
+#include <algorithm>
 #include <functional>  // for std::plus
 #include <iostream>    // for std::cout
 #include <numeric>     // for std::accumulate
@@ -10,30 +10,35 @@
 
 int main() {
   using namespace Exercise2;
-  vector_t numbers = {10, 5, 15, 12};
+  std::vector<int> v = {10, 5, 15, 12};
 
   // Calculate print maximum value
-  value_t highest = myMax(numbers);
+  int highest = *std::max_element(v.begin(), v.end());
   std::cout << "The highest number: " << highest << '\n';
 
   // Calculate and print average
-  double avg = myAvg(numbers);
+  double sum = std::accumulate(v.begin(), v.end(), 0.0);
+  double avg = sum / v.size();
   std::cout << "The average of the numbers: " << avg << '\n';
 
   // Make a copy of the array
-  vector_t outNumbers = myCopy(numbers);
+  std::vector<int> outNumbers(v);  // copy constructor
   std::cout << "The original array: ";
   // Print the copied array
-  printNumbers(outNumbers);
+  for (int n : outNumbers) std::cout << n << ' ';
+  std::cout << '\n';
 
   // Reverse the array in place, and print it
-  myReverse(outNumbers);
+  std::reverse(outNumbers.begin(), outNumbers.end());
   std::cout << "The reverse array: ";
-  printNumbers(outNumbers);
+  for (int n : outNumbers) std::cout << n << ' ';
+  std::cout << '\n';
 
   // Print the prime numbers in the array.
   std::cout << "The prime numbers in the array: ";
-  printPrimeNumbers(outNumbers);
+  std::copy_if(outNumbers.begin(), outNumbers.end(),
+               std::ostream_iterator<int>(std::cout, " "), isPrime);
+  std::cout << '\n';
 
   std::cout << '\n';  // final newline
   return 0;
@@ -43,35 +48,6 @@ namespace Exercise2 {
 
 // FUNCTION IMPLEMENTATIONS
 
-value_t myMax(const vector_t &a) {
-  auto it = a.begin();
-  value_t max = *it;
-  while (it++ != a.end()) {
-    if (*it > max) max = *it;
-  }
-  return max;
-}
-double myAvg(const vector_t &a) {
-  int count = a.size();
-  double sum = 0.0;
-  for (auto x : a) sum += x;
-  return sum / count;
-}
-
-inline vector_t myCopy(const vector_t &a) { return vector_t(a); }
-
-void printNumbers(const vector_t &a) {
-  for (const auto n : a) std::cout << n << ' ';
-  std::cout << '\n';
-}
-void myReverse(vector_t &a) {
-  // similar implementation to std::reverse from <algorithm> header
-  auto first = a.begin(), last = a.end();
-  while (first != last && first != --last) {  // performs size/2 swaps.
-    std::swap(*first, *last);
-    ++first;
-  }
-}
 bool isPrime(long num) {
   if (num < 2) return false;
   for (long i = 2; i * i <= num; i++)
