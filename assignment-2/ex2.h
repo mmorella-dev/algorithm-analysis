1aed03f93c695c1b6843a54ada9d0c6e85c13e60
morellam-dev <4561733+morellam-dev@users.noreply.github.com>
2021-02-02 11:22:22 -0500

    Slightly refactor the header file

--- assignment-2/ex2.h
+++ assignment-2/ex2.h
@@ -8,25 +8,27 @@
 
 // Returns the largest integer in the array
 // Precondition: size > 0, a has at least `size` elements
-int my_max(const int a[], size_t size);
+int my_max(const int arr[], size_t size);
 
 // Returns the average of the integers in the array
-// Precondition: size > 0, a has at least `size` elements
-double my_avg(const int a[], size_t size);
+// Precondition: size > 0, and arr has at least `size` elements
+double my_avg(const int arr[], size_t size);
 
-// Given an array, create a new
-int* my_copy(const int a[], size_t size);
+// Given an array, create a clone with the same size and same elements
+// Precondition: size > 0, and arr has at least `size` elements
+int* my_copy(const int arr[], size_t size);
 
 // Given an array, prints the integers in the indexes [0, size)
-void print_numbers(const int a[], size_t size);
+// Precondition: arr has at least `size` elements
+void print_numbers(const int arr[], size_t size);
 
 // Reverse the indices [0, size) of the array in place.
-void my_reverse(int a[], size_t size);
+void my_reverse(int arr[], size_t size);
 
-// Returns whether num is a prime number
-// i.e. there exists no integer i > 2 such that num/i is an integer.
-// Executes in O(n) time
+// Returns whether num is a prime number i.e. there exists no integer i > 2
+// such that num/i is an integer.
 bool isPrime(long num);
 
-//
-void print_prime_numbers(const int a[], size_t size);
+// Given an array, prints every integer in the indexes [0, size) which is a
+// prime number.
+void print_prime_numbers(const int arr[], size_t size);
