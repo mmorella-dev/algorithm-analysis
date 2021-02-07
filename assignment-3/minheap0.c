7ad9a47b53cc482c59e1f627a0be796b27f2752e
morellam-dev <4561733+morellam-dev@users.noreply.github.com>
2021-02-07 13:48:56 -0500

    Refactor heap implementation and comments

--- assignment-3/minheap0.c
+++ assignment-3/minheap0.c
@@ -9,7 +9,7 @@
 #include <stdio.h>
 #include <stdlib.h>  // for size_t
 
-// HELPER FUNCTIONS
+// HEAP FUNCTIONS
 // ================
 
 static inline size_t left(size_t i) { return i << 1; }
@@ -18,35 +18,20 @@ static inline size_t parent(size_t i) { return (i >> 1); }
 
 // Helper function which recursively maintains the heap property
 void min_heapify(value_t a[], size_t heap_size, size_t i) {
-  size_t l = left(i);
-  size_t r = right(i);
+  size_t l = (i << 1) + 1;
+  size_t r = (i << 1) + 2;
   size_t smallest = i;
-  if (l <= heap_size && a[l - 1] < a[smallest - 1]) {
+  if (l < heap_size && a[l] < a[smallest]) {
     smallest = l;
   }
-  if (r <= heap_size && a[r - 1] < a[smallest - 1]) {
+  if (r < heap_size && a[r] < a[smallest]) {
     smallest = r;
   }
   if (smallest != i) {
-    value_t temp = a[i - 1];
-    a[i - 1] = a[smallest - 1];
-    a[smallest - 1] = temp;
+    value_t temp = a[i];
+    a[i] = a[smallest];
+    a[smallest] = temp;
     min_heapify(a, heap_size, smallest);
-  }
-}
-
-// Helper function for inserting a new node
-void heap_decrease_key(value_t heap[], size_t i, value_t key) {
-  if (key > heap[i - 1]) {
-    fprintf(stderr, "Error: new key is greater than current key");
-    return;
-  }
-  heap[i - 1] = key;
-  while (i > 1 && heap[parent(i) - 1] > heap[i - 1]) {
-    value_t temp = heap[i - 1];
-    heap[i - 1] = heap[parent(i) - 1];
-    heap[parent(i) - 1] = temp;
-    i = i / 2;
   }
 }
 
@@ -54,7 +39,7 @@ void heap_decrease_key(value_t heap[], size_t i, value_t key) {
 // ========================
 
 void build_min_heap(value_t a[], size_t length) {
-  for (int i = length / 2; 1 <= i; --i) {
+  for (int i = length / 2; 0 <= i; --i) {
     min_heapify(a, length, i);
   }
 }
@@ -66,27 +51,40 @@ void min_heapsort(value_t a[], size_t length) {
     a[i - 1] = a[0];
     a[0] = temp;
     length--;
-    min_heapify(a, length, 1);
+    min_heapify(a, length, 0);
   }
 }
 
-value_t heap_minimum(value_t heap[], size_t size) {
-  return heap[0];
+// Helper function for inserting a new node
+void heap_decrease_key(value_t a[], size_t i, value_t key) {
+  if (key > a[i]) {
+    fprintf(stderr, "Error: new key is greater than current key");
+    return;
+  }
+  a[i] = key;
+  while (i > 0 && a[(i + 1) / 2 - 1] > a[i]) {
+    size_t parent = (i + 1) / 2 - 1;
+    value_t temp = a[i];
+    a[i] = a[parent];
+    a[parent] = temp;
+    i = parent;
+  }
 }
 
-value_t heap_extract_min(value_t heap[], size_t size) {
-  if (size < 1) {
-    fprintf(stderr, "Error: heap underflow. Exiting...");
+value_t heap_minimum(value_t a[], size_t heap_size) { return a[0]; }
+
+value_t heap_extract_min(value_t a[], size_t heap_size) {
+  if (heap_size < 1) {
+    fprintf(stderr, "Error: heap underflow. Exiting...\n");
     exit(EXIT_FAILURE);
   }
-  value_t min = heap[0];
-  heap[0] = heap[--size - 1];
-  min_heapify(heap, size, 1);
+  value_t min = a[0];
+  a[0] = a[--heap_size - 1];
+  min_heapify(a, heap_size, 0);
   return min;
 }
 
-void heap_insert(value_t heap[], size_t size, value_t key) {
-  size++;
-  heap[size - 1] = MIN_HEAP_MAX_NUMBER;
-  heap_decrease_key(heap, size, key);
+void heap_insert(value_t a[], size_t heap_size, value_t key) {
+  a[heap_size] = MIN_HEAP_MAX_NUMBER;
+  heap_decrease_key(a, heap_size, key);
 }
--- assignment-3/minheap0.h
+++ assignment-3/minheap0.h
@@ -7,8 +7,8 @@
 #include <limits.h>  // for INT_MAX
 #include <stdlib.h>  // for size_t
 
-typedef int value_t;  // the number type
-#define MIN_HEAP_MAX_NUMBER INT_MAX
+typedef int value_t;  // the number type used inside the queue
+#define MIN_HEAP_MAX_NUMBER INT_MAX // the largest number for the above type
 
 // MINIMUM HEAP FUNCTIONS
 // ======================
@@ -23,18 +23,16 @@ void build_min_heap(value_t a[], size_t length);
 // Postcondition: `a` is now sorted in place.
 void min_heapsort(value_t a[], size_t length);
 
-// MIN-PRIORITY QUEUE FUNCTIONS
-// ============================
-
+// Returns the smallest value in the heap.
 // Precondition: `a` is a min-heap with at least one element
-value_t heap_minimum(value_t heap[], size_t size);
+value_t heap_minimum(value_t a[], size_t heap_size);
 
-// Extracts the smallest value in the heap.
-// Precondition: a[] is a min-heap with at least one element
+// Extracts the smallest value in a heap.
+// Precondition: a[0...size) is a min-heap
 // Postcondition: heap_size -= 1
-value_t heap_extract_min(value_t heap[], size_t heap_size);
+value_t heap_extract_min(value_t a[], size_t heap_size);
 
 // Inserts a new element into a min heap
-// Precondition:  a[] is a min heap, with a capacity of at least size + 1
+// Precondition:  a[0...size) is a min heap. a is defined for a[0...size+1)
 // Postcondition: heap_size is now increased by 1
 void heap_insert(value_t a[], size_t heap_size, value_t key);
