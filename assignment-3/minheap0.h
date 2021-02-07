4a63419417135d19f2c07bb2a0baa6e4a59fcb12
morellam-dev <4561733+morellam-dev@users.noreply.github.com>
2021-02-07 15:57:26 -0500

    Refactor comment on minheap0.h

--- assignment-3/minheap0.h
+++ assignment-3/minheap0.h
@@ -1,7 +1,11 @@
 // FILE:   minheap0.h
 // AUTHOR: Mae Morella
 // ===================
-// Explain here
+// Defines functions for creating and manipulating min-heaps.
+// A min-heap is defined as a binary tree such that each node is smaller than
+// its children. An array A contains a min-heap if for any index i in [0,
+// A.size), the child elements at A[i*2+1] and A[i*2+2] are greater than A[i],
+// or do not exist.
 
 #include <limits.h>  // for INT_MAX
 #include <stdlib.h>  // for size_t
@@ -24,13 +28,8 @@ void min_heapsort(value_t a[], size_t length);
 
 // Produces a minimum-heap from an unordered input array.
 // Precondition: `a` is an unsorted array
-// Postcondition: `a` is now in min-heap order.
+// Postcondition: a[0, size) are now in min-heap order.
 void min_heap_build(value_t a[], size_t length);
-
-// Precondition: If they exist, the binary trees rooted at a[2i+1] and a[2i+2]
-// are min-heaps. heap_size < i.
-// Postcondition: The binary tree rooted at a[i] is a min-heap.
-void min_heapify(value_t a[], size_t heap_size, size_t i);
 
 // Returns the smallest value in the heap.
 // Precondition: `a` is a min-heap with at least one element
@@ -49,3 +48,8 @@ void min_heap_insert(value_t a[], size_t heap_size, value_t key);
 // Assigns the value at a[i] to `key` while maintaining the min-heap property.
 // Precondition: `key` is smaller than the previous value at a[i].
 void heap_decrease_key(value_t a[], size_t i, value_t key);
+
+// Precondition: If they exist, the binary trees rooted at a[2i+1] and a[2i+2]
+// are min-heaps. heap_size < i.
+// Postcondition: The binary tree rooted at a[i] is a min-heap.
+void min_heapify(value_t a[], size_t heap_size, size_t i);
