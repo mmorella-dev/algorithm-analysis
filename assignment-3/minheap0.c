1b2531ee1829650e64c9717d3f477217dc6a35a6
morellam-dev <4561733+morellam-dev@users.noreply.github.com>
2021-02-07 14:47:32 -0500

    Change left() and right() helper function behavior

--- assignment-3/minheap0.c
+++ assignment-3/minheap0.c
@@ -12,14 +12,15 @@
 // HEAP FUNCTIONS
 // ================
 
-static inline size_t left(size_t i) { return i << 1; }
-static inline size_t right(size_t i) { return (i << 1) + 1; }
-static inline size_t parent(size_t i) { return (i >> 1); }
+// Helper functions for heap indexes
+size_t left(size_t i) { return i * 2 + 1; }
+size_t right(size_t i) { return i * 2 + 2; }
+size_t parent(size_t i) { return (i + 1) / 2 - 1; }
 
 // Helper function which recursively maintains the heap property
 void min_heapify(value_t a[], size_t heap_size, size_t i) {
-  size_t l = (i << 1) + 1;
-  size_t r = (i << 1) + 2;
+  size_t l = left(i);
+  size_t r = right(i);
   size_t smallest = i;
   if (l < heap_size && a[l] < a[smallest]) {
     smallest = l;
@@ -62,12 +63,11 @@ void heap_decrease_key(value_t a[], size_t i, value_t key) {
     return;
   }
   a[i] = key;
-  while (i > 0 && a[(i + 1) / 2 - 1] > a[i]) {
-    size_t parent = (i + 1) / 2 - 1;
+  while (i > 0 && a[parent(i)] > a[i]) {
     value_t temp = a[i];
-    a[i] = a[parent];
-    a[parent] = temp;
-    i = parent;
+    a[i] = a[parent(i)];
+    a[parent(i)] = temp;
+    i = parent(i);
   }
 }
 
