84d19eef6a49e07c6e28ae50ab132ddc50018dc2
morellam-dev <4561733+morellam-dev@users.noreply.github.com>
2021-02-07 16:23:12 -0500

    Rewrite main.c to include tests for heap and queue

--- assignment-3/main.c
+++ assignment-3/main.c
@@ -1,26 +1,101 @@
-#include <stddef.h>
-#include <stdio.h>
+// FILE:   main.c
+// AUTHOR: Mae Morella
+// ===================
+
+// Implements a test program for the min_heap and priority_queue data structures
+// defined in minheap0.h
+
+#include <assert.h>   // for assert
+#include <stdbool.h>  // for boolean type
+#include <stdio.h>    // for printf function
+#include <stdlib.h>   // for size_t, rand, EXIT_SUCCESS
+#include <time.h>     // for time
 
 #include "minheap0.h"
-// #include "priorityqueue.h"
+#include "priority_queue.h"
 
-void print_array(int a[], size_t size) {
-  for (int i = 0; i < size; i++) printf("%d ", a[i]);
+// Helper function to print an array
+// Prints out the array elements at arr[0, size)
+void print_array(int arr[], size_t size) {
+  for (int i = 0; i < size; i++) printf("%2d ", arr[i]);
   printf("\n");
 }
 
-int main() {
-  int nums[] = {8, 6, 7, 5, 3, 0, 9, 80, 60, 70, 50, 30, 0, 90};
-  const size_t nums_size = sizeof(nums) / sizeof(nums[0]);
-  print_array(nums, nums_size);
-
-  // build_min_heap(nums, nums_size);
-  min_heapsort(nums, nums_size);
-  print_array(nums, nums_size);
-
-  build_min_heap(nums, nums_size);
-  int heap_size = nums_size;
-  while (--heap_size > 0) {
-    printf("%d\n", heap_extract_min(nums, heap_size));
+// Helper function:
+// Checks whether the given array is in heap order.
+bool is_heap(int arr[], size_t i, size_t size) {
+  size_t l = 2 * i + 1;
+  size_t r = 2 * i + 2;
+  if (r >= size) {
+    return true;
   }
+  if (arr[i] <= arr[l] && arr[i] <= arr[r]) {
+    return is_heap(arr, l, size) && is_heap(arr, r, size);
+  }
+  return false;
+}
+
+// Helper function:
+// Checks whether the given array is in descending order
+bool is_descending(int arr[], size_t size) {
+  for (int i = 1; i < size; i++) {
+    if (arr[i] > arr[i - 1]) return false;
+  }
+  return true;
+}
+
+int main() {
+  // Initialize an unsorted array of integers:
+  int nums[] = {8, 6, 7, 5, 3, 0, 9, 80, 60, 70, 50, 30, 0, 90};
+  const size_t nums_len = sizeof(nums) / sizeof(nums[0]);
+  printf("- Original array: ");
+  print_array(nums, nums_len);
+
+  // Test #1: put `nums` in heap order
+  printf("Test #1: Calling min_heap_build...\n");
+  min_heap_build(nums, nums_len);
+  printf(" -- Result: ");
+  print_array(nums, nums_len);
+  assert(is_heap(nums, 0, nums_len));
+  printf("✔️ Array is in heap order.\n\n");
+
+  // Test #2: The heapsorted array is in descending order:
+  printf("Test #2: Calling min_heapsort...\n");
+  min_heapsort(nums, nums_len);
+  printf(" -- Result: ");
+  print_array(nums, nums_len);
+  assert(is_descending(nums, nums_len));
+  printf("✔️ Array is sorted.\n\n");
+
+  // Test #3: Create a struct priority_queue.
+  printf("Test #3: Calling alloc_priority_queue...\n");
+  priority_queue *q_ptr = alloc_priority_queue(nums_len);
+  assert(q_ptr != NULL && q_ptr->size == 0);
+  printf("✔️ Priority queue allocated successfully.\n\n");
+
+  printf("Test #4: Inserting random elements...\n");
+  srand(time(NULL));
+  for (int i = 0; i < q_ptr->capacity; i++) {
+    assert(is_heap(q_ptr->arr, 0, q_ptr->size));
+    int value = rand() % 100 - 9;
+    printf("-- priority_queue_insert(%2d)... ", value);
+    priority_queue_insert(q_ptr, value);
+    print_array(q_ptr->arr, q_ptr->size);
+    // rigorously test that arr[0, size) is a heap after every operation
+  }
+  assert(is_heap(q_ptr->arr, 0, q_ptr->size));
+  printf("✔️ Priority queue is a heap.\n\n");
+
+  printf("Test #5: Popping elements from queue...\n");
+  while (q_ptr->size > 0) {
+    assert(is_heap(q_ptr->arr, 0, q_ptr->size));
+    printf("-- priority_queue_pop_min()... ");
+    priority_queue_pop_min(q_ptr);
+    print_array(q_ptr->arr, q_ptr->size);
+    // rigorously test that arr[0, size) is a heap after every operation
+  }
+  assert(is_heap(q_ptr->arr, 0, q_ptr->size));
+  printf("✔️ Priority queue is a heap.\n\n");
+
+  return EXIT_SUCCESS;
 }
